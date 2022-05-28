df17549998872a2d0cb2d3d41edb8d9ed011e456
Milos Potic <101191580+mpotic@users.noreply.github.com>
2022-05-28 18:27:48 +0200

    Writer factory
    Implemented creation of new writer.

---
+++ factories/writer_factory.py
@@ -0,0 +1,12 @@
+from modules.writer import Writer
+
+
+class WriterFactory:
+    @staticmethod
+    def MakeWriter(count, keys, writers):
+        new_writer_id = 1
+        for i in range(count):
+            while new_writer_id in keys:
+                new_writer_id += 1
+            new_writer = Writer(new_writer_id)
+            writers[new_writer_id] = new_writer
