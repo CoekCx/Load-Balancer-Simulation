355acda1743e2907e6dcd99b7f0038118347dfb4
Milos Potic <101191580+mpotic@users.noreply.github.com>
2022-05-29 23:36:26 +0200

    Fundamental write functionality

--- modules/writer.py
+++ modules/writer.py
@@ -1,30 +1,72 @@
+import os
+
+from inquirer2 import prompt
+
 from modules.load_balancer import LoadBalancer
-from utils.color import print_in_color, Color, cursor
+from utils.color import in_color, Color, print_in_color, cursor
 
 
 class Writer:
-    def __init__(self, id, load_balancer):
+    def __init__(self, id):
         self.id = id
         self.active = False
+        self.__functions = {
+            'Show worker statuses': self.__ShowWorkerStatuses,
+            'Send data using Load Balancer': self.__SendData,
+            'Send data manually': self.__ManuallySendData,
+            'Create worker': self.__CreateWorker,
+            'Destroy workers': self.__DestroyWorkers,
+            'Manage workers': self.__ChangeWorkersStates,
+            'Close writer': self.__Close
+        }
 
-    def ShowWorkerStatuses(self):
+    def Execute(self):
+        self.active = True
+        self.__ExecuteCleanMethod(self.__UserPrompt)
+
+    def __UserPrompt(self):
+        choices = ['Show worker statuses', 'Send data using Load Balancer', 'Send data manually', 'Create worker',
+                   'Destroy workers', 'Manage workers', 'Close writer']
+        questions = [
+            {
+                'type': 'list',
+                'name': 'user_input',
+                'message': f"|System>>Writer {self.id}>",
+                'choices': choices,
+            }
+        ]
+        answers = prompt.prompt(questions)
+
+        self.__ExecuteCleanMethod(self.__functions[answers['user_input']])
+
+    @staticmethod
+    def __ShowWorkerStatuses():
         if len(LoadBalancer.workers) == 0:
             print_in_color('No workers to show', Color.RED)
         for worker in LoadBalancer.workers.values():
             print(worker)
         cursor()
+        input()
 
-    def SendData(self):
+    def __SendData(self):
         pass
 
-    def ManuallySendData(self):
+    def __ManuallySendData(self):
         pass
 
-    def CreateWorker(self):
+    def __CreateWorker(self):
         pass
 
-    def DestroyWorkers(self):
+    def __DestroyWorkers(self):
         pass
 
-    def ChangeWorkersStates(self):
+    def __ChangeWorkersStates(self):
         pass
+
+    def __Close(self):
+        pass
+
+    @staticmethod
+    def __ExecuteCleanMethod(method):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        method()
