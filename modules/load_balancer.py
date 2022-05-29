04fb30074cd147aca2ad363cbea5a89ca510dc3b
Djordje123456 <101287757+Djordje123456@users.noreply.github.com>
2022-05-29 19:53:23 +0200

    Implemented some load balancer functionality

--- modules/load_balancer.py
+++ modules/load_balancer.py
@@ -1,12 +1,21 @@
+from config import BUFFER_SIZE
+from modules.database import Database
+
+
 class LoadBalancer:
     buffer = []
     workers = {}
 
     def ReceiveData(self, data_entry):
-        pass
+        LoadBalancer.buffer.append(data_entry)
+        if len(LoadBalancer.buffer) < BUFFER_SIZE:
+            return
+
+        self.ProcessData()
 
     def ProcessData(self):
-        pass
+        for data_entry in LoadBalancer.buffer:
+            pass  # Process data with worker here when it is implemented
 
     def GetWorker(self):
         pass
